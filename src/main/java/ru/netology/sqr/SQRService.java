e611f59deda7776db4ccc71c45f7756a46fea581
nrr <nrr@warsong.me>
2022-05-07 11:15:55 +0300

    init commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int sqr(int firstNumber, int secondNumber) {
+        int count = 0;
+
+        for (int i = 10; i <= 99; i++) {
+            if ((i * i) >= firstNumber && (i * i) <= secondNumber) count++;
+        }
+
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvSource({
+            //"testName,firstNumber,secondNumber,expRes"
+            "200-300,200,300,3",
+            "100-10000,100,10000,90",
+            "300-500,300,500,5",
+            "600-800,600,800,4",
+            "1000-2000,1000,2000,13",
+            "failtest,100,1000,2"
+    })
+    public void test(String testName, int firstNumber, int secondNumber, int expRes) {
+        SQRService service = new SQRService();
+
+        int actual = service.sqr(firstNumber, secondNumber);
+
+        Assertions.assertEquals(expRes, actual);
+    }
+}
